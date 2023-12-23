174db5ee5dd977a93d25e184f2a90a23ca4de8e1
siw-ss <siwarsoula@gmail.com>
2023-12-23 22:37:36 +0100

    first commit

---
+++ src/main/java/com/example/seventeen/SeventeenApplication.java
@@ -0,0 +1,13 @@
+package com.example.seventeen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeventeenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeventeenApplication.class, args);
+	}
+
+}
