d21fbda4c14556b6748c6bb491f4fd76294b731e
siw-ss <siwarsoula@gmail.com>
2023-12-24 16:57:15 +0100

    added simple REST API (/welcome)

--- src/main/java/com/example/seventeen/SeventeenApplication.java
+++ src/main/java/com/example/seventeen/SeventeenApplication.java
@@ -2,10 +2,17 @@ package com.example.seventeen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SeventeenApplication {
 
+	@GetMapping("/welcome")
+	public String welcome(){
+		return "Spring Boot Docker Demo";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(SeventeenApplication.class, args);
 	}
