a127f328821cd292d05c99f0a885aac19227c65d
siw-ss <siwarsoula@gmail.com>
2023-12-25 02:57:47 +0100

    added sonarqube + increased code coverage

--- src/test/java/com/example/seventeen/SeventeenApplicationTests.java
+++ src/test/java/com/example/seventeen/SeventeenApplicationTests.java
@@ -3,11 +3,16 @@ package com.example.seventeen;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class SeventeenApplicationTests {
 
 	@Test
 	void contextLoads() {
+		SeventeenApplication app = new SeventeenApplication();
+		assertEquals("Spring Boot Docker Demo",app.welcome());
 	}
 
 }
