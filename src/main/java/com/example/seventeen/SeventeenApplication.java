8cf29511803fd27550b8185ad3be73b73c66bf3f
siw-ss <siwarsoula@gmail.com>
2024-01-18 23:17:54 +0100

    connect to front

--- src/main/java/com/example/seventeen/SeventeenApplication.java
+++ src/main/java/com/example/seventeen/SeventeenApplication.java
@@ -2,9 +2,11 @@ package com.example.seventeen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @SpringBootApplication
 @RestController
 public class SeventeenApplication {
